f20c5f06b4117f09b477ae6ebc394049a8e3070b
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2024-01-26 23:18:18 +0100

    migration table

--- backend/database/migrations/2024_01_23_210542_create_products_table.php
+++ backend/database/migrations/2024_01_23_210542_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->integer('size');
+            $table->string('size');
             $table->float("price");
             $table->timestamps();
         });
