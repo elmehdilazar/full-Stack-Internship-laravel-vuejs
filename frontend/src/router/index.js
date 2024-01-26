a648f1ccb413083f3df74ab4c775bff7c08bea23
elmehdilazar <96011628+elmehdilazar@users.noreply.github.com>
2024-01-26 23:28:57 +0100

    frontend routes and importation

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,35 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddProductViewVue from '@/views/AddProductView.vue'
+import UpdateProductVue from '@/views/UpdateProduct.vue'
+import SignInVue from '@/views/SignIn.vue'
+import SignUpVue from '@/views/SignUp.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/addProduct',
+    name: 'addproduct',
+    component:AddProductViewVue
+  },
+  {
+    path: '/updateProduct/:idprod',
+    name: 'UpdateProduct',
+    component:UpdateProductVue
+  },
+  {
+    path: '/sign',
+    name: 'signin',
+    component:SignInVue
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component:SignUpVue
   },
   {
     path: '/about',
